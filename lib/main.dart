902601d2c06ebb7c64f0a98f5bd59f9a97ba4d9e
MeenaSivakumar <meenasad09@gmail.com>
2024-10-10 16:19:29 +0530

    sevan

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 import 'package:work_zone/controllers/auth_controller.dart';
 import 'views/worker/auth/worker_signup_page.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+   await Future.delayed(const Duration(seconds: 2));
+  FlutterNativeSplash.remove();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
--- lib/views/worker/home/worker_home_page.dart
+++ lib/views/worker/home/worker_home_page.dart
@@ -8,25 +8,35 @@ class WorkerHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.backgroundColor,
-      appBar: AppBar(
-        backgroundColor: AppTheme.primaryColor,
-        centerTitle: true,
-        title: Text(
-          'WorkZone',
-          style: AppTheme.themeData.textTheme.headlineLarge!.copyWith(
-            fontSize: 20,
-            color: AppTheme.blackColor,
-            fontWeight: FontWeight.bold,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(100.0), // Adjust height as needed
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFF037AA8), // Use your app bar color
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30.0), // Rounded corners
+              bottomRight: Radius.circular(30.0),
+            ),
+          ),
+          child: const SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(children: [Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/user_profile.svg'),
+                      radius: 25.0,
+                    ),
+                  ),
+                  SizedBox(width: 10.0),],)
+                ],
+              ),
+            ),
           ),
         ),
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.notifications,
-                color: AppTheme.blackColor,
-              ))
-        ],
       ),
     );
   }
